a880fe96721ba66f62fa6969da9971e8f3a41b85
Grayson Nocera <gnocera@purdue.edu>
2022-11-23 17:51:27 -0600

    Add files via upload

---
+++ build/precache-manifest.c9bb9a6c37c12d9e5f5870790fe81903.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1808f26617b92246bcf",
+    "url": "/static/css/main.21e6f705.chunk.css"
+  },
+  {
+    "revision": "a1808f26617b92246bcf",
+    "url": "/static/js/main.a1808f26.chunk.js"
+  },
+  {
+    "revision": "eb664f6cc0ddc47ee766",
+    "url": "/static/js/1.eb664f6c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "85dee166ec295503525c62cd9af611fb",
+    "url": "/static/media/temple_run.85dee166.png"
+  },
+  {
+    "revision": "8daff847d4ae8161924fe2d7f0c3ba86",
+    "url": "/static/media/hindu_temple.8daff847.jpeg"
+  },
+  {
+    "revision": "8a64ecd4825462261b17562122b24c40",
+    "url": "/static/media/aztec_temple.8a64ecd4.jpeg"
+  },
+  {
+    "revision": "07cec7e2b00c793325e990a8f3e09ff3",
+    "url": "/static/media/aztec_serpent.07cec7e2.svg"
+  },
+  {
+    "revision": "57dd60e67b52faeecd9ad8691eb4c7b0",
+    "url": "/static/media/hindu_serpent.57dd60e6.jpeg"
+  },
+  {
+    "revision": "743551ef68597ca0f78143049ae5b5a2",
+    "url": "/static/media/golden_idol.743551ef.jpg"
+  },
+  {
+    "revision": "2db8eeba6eef7b913cbfb77ac797cfa9",
+    "url": "/static/media/evil_demon_monkey.2db8eeba.jpg"
+  },
+  {
+    "revision": "fee987353e50617479689100c64211d6",
+    "url": "/static/media/my_stats.fee98735.PNG"
+  },
+  {
+    "revision": "9e9ac1d480fc5c9a9e846d85e12be034",
+    "url": "/index.html"
+  }
+];
